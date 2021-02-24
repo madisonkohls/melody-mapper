a598d9dca81aa237f4d0f9c4e8ff563dfa28832f
kcsantoso <72245373+kcsantoso@users.noreply.github.com>
2021-02-23 18:13:14 -0800

    created mood page

--- src/components/mood.component.js
+++ src/components/mood.component.js
@@ -1,1 +1,50 @@
+import React, { useEffect, useState } from "react"
+import { useParams } from "react-router-dom"
+import axios from 'axios'
+import {Button} from 'reactstrap'
+import '../App.css';
 
+function Mood() {
+    const [ username, setUsername ] = useState("")
+    const [ firstName, setFirstName ] = useState("")
+    const [ emailAddress, setEmailAddress ] = useState("")
+    const [ musicGenre, setMusicGenre ] = useState(0)
+    let params = useParams()
+
+    useEffect(() => {
+        axios.get('http://localhost:8000/users/'+params.id)
+            .then(response => {
+                const user = response.data
+                setUsername(user.username)
+                setFirstName(user.firstName)
+                setEmailAddress(user.emailAddress)
+                setMusicGenre(user.musicGenre)
+            })
+            .catch(function(error){
+                console.log(error)
+            })
+    }, [])
+
+    let onHomePage = (event) => {
+        event.preventDefault();
+        console.log("Home Page")
+        window.location = '/'
+    }
+
+    return (
+      <div>
+        <div class = "mainpage">
+            <div class = "logoutbutton">
+            <Button style={{backgroundColor:"#4B7268"}} onClick={onHomePage}>Log out</Button>
+            </div>
+            <div class = "topsection">
+            <h2 class="titleText">hey {firstName}!</h2>
+            <p>Enter your mood here!</p>
+            </div>
+        </div>
+      </div>
+
+    )
+}
+
+export default Mood;
