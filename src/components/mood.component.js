001b964efff7e2e49afff31d9ad923a44095b912
kcsantoso <72245373+kcsantoso@users.noreply.github.com>
2021-02-25 13:56:35 -0800

    Update mood.component.js

--- src/components/mood.component.js
+++ src/components/mood.component.js
@@ -8,7 +8,8 @@ function Mood() {
     const [ username, setUsername ] = useState("")
     const [ firstName, setFirstName ] = useState("")
     const [ emailAddress, setEmailAddress ] = useState("")
-    const [ musicGenre, setMusicGenre ] = useState(0)
+    const [ musicGenre, setMusicGenre ] = useState("")
+    const [ mood, setMood ] = useState(0)
     let params = useParams()
 
     useEffect(() => {
@@ -19,6 +20,7 @@ function Mood() {
                 setFirstName(user.firstName)
                 setEmailAddress(user.emailAddress)
                 setMusicGenre(user.musicGenre)
+                setMood(user.mood)
             })
             .catch(function(error){
                 console.log(error)
@@ -29,6 +31,56 @@ function Mood() {
         event.preventDefault();
         console.log("Home Page")
         window.location = '/'
+    }
+    
+    function onMoodClick1 (event) {
+        event.preventDefault();
+        updateAccount("happy");
+    }
+
+    let updateAccount = (mood) => {
+        const user = {
+            username: username,
+            password: password,
+            firstName: firstName,
+            emailAddress: emailAddress,
+            musicGenre: genre
+            mood: mood
+            
+        }
+
+        console.log(user)
+
+        axios.post('http://localhost:8000/users/updateaccount/'+params.id, user)
+            .then(res => handleSuccess(res))
+            .catch(error => handleErrorEdit(error))
+    }
+
+    let handleSuccess = (res) => {
+        console.log(res)
+        console.log('Successful Response')
+        window.location = '/profile/'+res.data.user._id
+    }
+
+    let handleErrorEdit = (error) => {
+        let errorMessage
+        if(error.response && error.response.data) {
+            //Error Responses
+            if(error.response.data.type){
+                errorMessage = error.response.data.type
+            }
+            else {
+                errorMessage = error.response.data
+            }
+        } else if(error.request) {
+            // Network failures, etc.
+            errorMessage = error.request.message || error.request.statusText;
+        } else {
+            //Other errors
+            errorMessage= error.message
+        }
+        console.log(errorMessage)
+        alert(errorMessage)
     }
 
     return (
@@ -42,7 +94,7 @@ function Mood() {
             <p>How are you feeling today?</p>
             </div>
             <div class = "moodbutton">
-            <Button style={{backgroundColor:"#CCFFCC"}} onClick={onHomePage}>Good :)</Button>
+            <Button style={{backgroundColor:"#CCFFCC"}} onClick={onMoodClick1}>Good :)</Button>
             </div>
             <div class = "moodbutton">
             <Button style={{backgroundColor:"#FFFFCC"}} onClick={onHomePage}>Neutral :/</Button>
