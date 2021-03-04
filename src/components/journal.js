af9fd02e90d8e796913dcfefeadabf52de19dcd2
Nisha McNealis <nisha.mcnealis@gmail.com>
2021-03-04 12:29:41 -0800

    style changes, added search button

--- src/components/journal.js
+++ src/components/journal.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react"
 import Entries from "./entries"
 import Form from "./form"
-import SearchField from "react-search-field";
+import {Button, FormGroup, Input, Label} from 'reactstrap'
+
 
 //import axios from 'axios'
 
@@ -141,9 +142,11 @@ class Journal extends Component {
       currBody: "",
     })
   }
-  handleSearchInputChange = entry => {
-    //do the search
+  handleSearchInputChange = () => {
+    //do the search for this.state.searchContents
     //this.setState({ entries: //search results
+    //it's currently set up so the search will happen whenever the search field input changes
+    //or the user presses the "Search" button
   }
 
   componentDidMount() {
@@ -155,24 +158,26 @@ class Journal extends Component {
     const { entries } = this.state;
     return (
       <div class = "journal">
-
-
         <div class = "journalelement">
-
-        <h2 class = "titleText"> {this.state.addEdit} entry </h2>
-        <Form handleSubmit={this.handleSubmit} clearForm={this.clearForm} userid={this.userID} title={this.state.currTitle} body = {this.state.currBody}/>        </div>
-
+          <h2 class = "titleText"> {this.state.addEdit} entry </h2>
+          <Form handleSubmit={this.handleSubmit} clearForm={this.clearForm} userid={this.userID} title={this.state.currTitle} body = {this.state.currBody}/>
+        </div>
         <div class = "journalelement">
          <h2 class = "titleText"> search all entries </h2>
           <div class = "searchcontainer">
-              <SearchField
-                placeholder="search entries by title"
-                onChange={this.handleSearchInputChange}
-                searchText=""
-                classNames="searchbar"
-                />
-            </div>
-            <Entries entryData={entries} removeEntry={this.removeEntry} editEntry={this.editEntry}/>
+              <div class = "searchelement">
+                <Input
+                  placeholder="Search entries by title."
+                  onChange={this.handleSearchInputChange}
+                  className="searchbar"
+                  value = {this.state.searchContents}
+                  />
+              </div>
+              <div class = "searchelement">
+                <Button style={{backgroundColor:"#4B7268"}} onClick={this.handleSearchInputChange}>Search</Button>
+              </div>
+          </div>
+          <Entries entryData={entries} removeEntry={this.removeEntry} editEntry={this.editEntry}/>
         </div>
       </div>
     )
