ecf76ed3349badfd9ed0e87e0462292923a17809
Nisha McNealis <nisha.mcnealis@gmail.com>
2021-03-07 21:44:14 -0800

    fix extra html stuff

--- src/components/journal.js
+++ src/components/journal.js
@@ -114,13 +114,6 @@ class Journal extends Component {
               </div>
               <div class = "searchelement">
                 <Button style={{backgroundColor:"#4B7268"}} onClick={() => this.handleSearch(this.state.searchContents)} >Search</Button>
-                  onChange={this.handleSearchInputChange}
-                  className="searchbar"
-                  value = {this.state.searchContents}
-                  />
-              </div>
-              <div class = "searchelement">
-                <Button style={{backgroundColor:"#4B7268"}} onClick={this.handleSearchInputChange}>Search</Button>
               </div>
           </div>
           <Entries entryData={entries} removeEntry={this.removeEntry} editEntry={this.editEntry}/>
